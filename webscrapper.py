701a8a2d021ff20bab0b6ff47a82aa87f2e73eed
realy1x <60939601+realy1x@users.noreply.github.com>
2022-09-10 00:42:34 +0300

    requirements

--- webscrapper.py
+++ webscrapper.py
@@ -55,9 +55,13 @@ def main(argv):
         writer.writerow([title_list[w], price_list[w]])
     #writer.writerow([price_list])
     
+    
 
 if __name__ == "__main__":
     main(sys.argv)
+
+
+
 #main('https://www.amazon.com/s?k=hhd&crid=1LBCOBUKIJS3W&sprefix=hh%2Caps%2C192&ref=nb_sb_noss_2')
 #main('https://www.amazon.com/s?k=ssd&crid=191KTLBDKLR17&sprefix=ss%2Caps%2C748&ref=nb_sb_noss_2')  
 #main('https://www.amazon.com/b?node=16225016011&pf_rd_r=YTVZMPFEP9YXHZAHDBV2&pf_rd_p=e5b0c85f-569c-4c90-a58f-0c0a260e45a0&pd_rd_r=ad2d4c1f-6b53-4e70-be94-796cabecd1d9&pd_rd_w=7WaiY&pd_rd_wg=3Vwk0&ref_=pd_gw_unk')
